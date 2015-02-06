7f96cc452772d9676ebbfb0e8ff52ee413b2e068
dusin-x <dusin-x@users.noreply.github.com>
2015-02-06 20:43:22 +0800

    add a new line from site
    add  a new line from site

--- src/hellogithub/HelloGithub.java
+++ src/hellogithub/HelloGithub.java
@@ -15,5 +15,6 @@ public class HelloGithub {
      */
     public static void main(String[] args) {
         System.out.println("hello GitHub");
+        System.out.println("add from github site");
     }
 }
